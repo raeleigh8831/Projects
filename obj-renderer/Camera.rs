5cc629084dd3691d4a74a9b32604caab35576107
raeleigh8831 <raeleigh8831@gmail.com>
2025-12-21 16:23:11 +0000

    Create Camera.rs

---
+++ obj-renderer/Camera.rs
@@ -0,0 +1,19 @@
+use glam::*;
+
+pub struct Camera {
+    pub eye: Vec3,
+    pub target: Vec3,
+    pub up: Vec3,
+    pub aspect: f32,
+    pub fovy: f32,
+    pub znear: f32,
+    pub zfar: f32,
+}
+
+impl Camera {
+    pub fn build_view_projection_matrix(&self) -> Mat4 {
+        let view = Mat4::look_at_rh(self.eye, self.target, self.up);
+        let proj = Mat4::perspective_rh(self.fovy, self.aspect, self.znear, self.zfar);
+        proj * view
+    }
+}
